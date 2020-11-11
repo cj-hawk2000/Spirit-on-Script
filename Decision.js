378b55ce3d0d2e8e08ed7b6e1acb7d4377972f8e
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2020-11-11 23:25:30 +0530

    Update Decision.js

--- Decision.js
+++ Decision.js
@@ -2,4 +2,11 @@
 console.log("Decision making");
 var str1="Hello world";
 var str2="Good bye";
+if (str1==="Hello world"){
+  alert("Hello world !!!");
+}
+else{
+  alert("Desicion Making");
+}
+
 
